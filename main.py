c9d2a58a01a63ea04ec2adfd730c6a7db68d4a05
Géza Ahsendorf <gezaahs@gmail.com>
2025-01-28 13:06:01 +0100

    run from main.py

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-import pygame
+import renderer.renderer as renderer
 
-class Enemy:
-    def __init__(self, name):
-        self.name = name
-
-class FluteGuy(Enemy):
-    def __init__(self, name):
-        self.name = name
-
-pygame.init()
-
-canvas = pygame.display.set_mode((0,0), pygame.FULLSCREEN, pygame.RESIZABLE)
-
-pygame.display.set_caption("DOOT")
-mainLoop = True
-
-while mainLoop:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            mainLoop = False
-        if pygame.mouse.get_pressed()[2] == True:
-            print("Autsch")
-            pygame.display.toggle_fullscreen()
-    pygame.display.update
-
-pygame.quit()
+renderer.run_event_loop()
--- renderer/renderer.py
+++ renderer/renderer.py
@@ -1,6 +1,6 @@
 import moderngl as gl
 import moderngl_window as glw
-import window
+import renderer.window as window
 
 def run_event_loop():
     glw.run_window_config(window.Window)
